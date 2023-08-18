d96803d173a46aa6abda4377273e3a14693eef2a
Arpit <arpitsengar99@gmail.com>
2023-08-19 02:22:17 +0530

    Update scraper.py

--- insta/scraper.py
+++ insta/scraper.py
@@ -23,14 +23,15 @@ response = requests.get(url, headers=headers, params=querystring).json()
 
 
 try:
-	bio = response[0]["bio_links"]["url"]
-except KeyError:
-	bio = ""
-	
-print(f"""
-      
+	try:
+		bio = response[0]["bio_links"]["url"]
+	except KeyError and TypeError:
+		bio = ""
+		
+	print(f"""
+		
 --------------------------------------------------------
-      
+	
 Full Name : {response[0]["full_name"]}
 Username : {response[0]["username"]}
 Follower Count : {response[0]["follower_count"]}
@@ -40,17 +41,20 @@ Pronouns : {response[0]["pronouns"]}
 Bio : {response[0]["biography"]}
 Bio Links : {bio}
 
+Media Count : {response[0]["media_count"]}
+
 New to Instagram : {response[0]["is_new_to_instagram"]}
 Private : {response[0]["is_private"]}
 Verified : {response[0]["is_verified"]}
 Business : {response[0]["is_business"]}
 Professional : {response[0]["is_professional"]}
 
-Media Count : {response[0]["media_count"]}
-
 Profile Pic URL : {response[0]["profile_pic_url"]}\n
 HD Profile Pic URL : {response[0]["profile_pic_url_hd"]}\n
 
 --------------------------------------------------------
 
-""")
+	""")
+
+except KeyError:
+	print(response["message"])
