cc2bf290b41446a117664472cb7343e67d69b697
Arpit <arpitsengar99@gmail.com>
2023-08-19 02:00:52 +0530

    Update scraper.py

--- insta/scraper.py
+++ insta/scraper.py
@@ -24,20 +24,22 @@ response = requests.get(url, headers=headers, params=querystring).json()
 
 print(f"""
 --------------------------------------------------------\n
-Username : {response[0]["username"]}
 Full Name : {response[0]["full_name"]}
+Username : {response[0]["username"]}
 Follower Count : {response[0]["follower_count"]}
 Following Count : {response[0]["following_count"]}
-
-Media Count : {response[0]["media_count"]}
+Pronouns : {response[0]["pronouns"]}
 
 Bio : {response[0]["biography"]}
 Bio Links : {response[0]["bio_links"]}
 
+New to Instagram : {response[0]["is_new_to_instagram"]}
 Private : {response[0]["is_private"]}
+Verified : {response[0]["is_verified"]}
 Business : {response[0]["is_business"]}
 Professional : {response[0]["is_professional"]}
-New to Instagram : {response[0]["is_new_to_instagram"]}
+
+Media Count : {response[0]["media_count"]}
 
 Profile Pic URL : {response[0]["profile_pic_url"]}\n
 HD Profile Pic URL : {response[0]["profile_pic_url_hd"]}\n
